1de6c231e96eed78552ba32163aa033857b930ff
m-kazi <tanim703@gmail.com>
2023-12-06 22:46:30 +0600

    Used MVC to update the checkout header

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 //Calling the function at the start of the page
+renderCheckoutHeader();
+
 renderOrderSummary();
 
 renderPaymentSummary();
